da3d5d7c5076d61a5df55dcfe97b03ad48bf5c74
Dai <jipengda@buffalo.edu>
2019-10-16 14:52:08 -0400

    Battery Capacity Constraint

--- Help.py
+++ Help.py
@@ -14,3 +14,10 @@ r_to_degree = 180/(math.pi)
 grama_new = garma*(r_to_degree)
 lamda = round(lamda, 1)
 garma_new = round(garma_new, 1)
+
+cagents = [(i) for i in range(A)]
+cnodes = [(j) for j in range(N)]
+acnodes = [(i,j) for i in cagents for j in cnodes]
+arcos=[(i,j) for i in cnodes for j in cnodes if i!=j]
+double=[(i,j) for i in cnodes for j in cnodes if i!=j]
+triple=[(i,j,k) for i in ]
