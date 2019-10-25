8a2e18dc64765ee45a952e3b7927a94497a07a67
David Dai <jipeng@davids-air.home>
2019-10-25 00:04:00 -0400

    Python converted.

--- Ugly Numbers.py
+++ Ugly Numbers.py
@@ -17,47 +17,45 @@
 
 # Recommened: Please solve it on "PRACTICE" first, before moving on to
 # the solution
-
-long long int uglyNumber(int n){
-        // f2 maintains the index of the ugly number
-        // for multiplication by 2
-        int f2 = 1;
-        // f3 maintains the index of the ugly number
-        // for multiplication by 3
-        int f3 = 1;
-        // f5 maintains the index of the ugly number
-        // for multiplication by 5
-        int f5 = 1;
-        long long int dp[n + 1] = {0};
-        // first ugly number is 1
+import numpy as np
+def uglyNumber(n):
+        # f2 maintains the index of the ugly number
+        # for multiplication by 2
+        f2 = 1;
+        # f3 maintains the index of the ugly number
+        # for multiplication by 3
+        f3 = 1;
+        # f5 maintains the index of the ugly number
+        # for multiplication by 5
+        f5 = 1;
+        dp = np.zeros(n+1, int)
+        # first ugly number is 1
         dp[1] = 1;
-        // filling the dp matrix from i = 2
+        # filling the dp matrix from i = 2
         for i in range(2, n+1):
-            // calculating the next number from previous ugly numbers
-            long long int ans_f_2 = dp[f2] * 2;
-            long long int ans_f_3 = dp[f3] * 3;
-            long long int ans_f_5 = dp[f5] * 5;
-            // finding minimum of all the answer
-            long long int final_ans = min(ans_f_2, min(ans_f_3, ans_f_5))
-            //storing the ans
+            # calculating the next number from previous ugly numbers
+            ans_f_2 = dp[f2] * 2;
+            ans_f_3 = dp[f3] * 3;
+            ans_f_5 = dp[f5] * 5;
+            # finding minimum of all the answer
+            final_ans = min(ans_f_2, min(ans_f_3, ans_f_5))
+            #storing the ans
             dp[i] = final_ans;
-            // if our final_ans is ans_f_2, ans_f_3, ans_f_5
-            // then increment their counters i.e f2, f3, f5
-            if(final_ans == ans_f_2) {
-                    f2++;
-            }
-            if(final_ans == ans_f_3) {
-                    f3++;
-            }
-            if(final_ans == ans_f_5){
-                    f5++;
-            }
-        return dp[n];
-}
+            # if our final_ans is ans_f_2, ans_f_3, ans_f_5
+            # then increment their counters i.e f2, f3, f5
+            if(final_ans == ans_f_2):
+                    f2 = f2 + 1;
 
-// driver program to check the code
-int main(){
-    int n;
-    n = Input(n); #??
-    print(n+"th ugly number is "+ uglyNumber(n))
-}
+            if(final_ans == ans_f_3):
+                    f3 = f3 + 1;
+
+            if(final_ans == ans_f_5):
+                    f5 = f5 + 1;
+        return dp[n];
+
+# driver program to check the code
+if __name__=="__main__":
+    n = input("Enter number n: ")
+    n = int(n)
+    uglyNumber = uglyNumber(n)
+    print("The %dth ugly number is %d" % (n, uglyNumber))
