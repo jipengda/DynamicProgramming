424a8b00daa10644fae469b00a1ca1dab38c9ea2
David Dai <jipeng@davids-air.home>
2019-10-26 23:05:39 -0400

    For example

--- Largest Sum Contiguous Subarray.py
+++ Largest Sum Contiguous Subarray.py
@@ -1,3 +1,6 @@
 # Write an efficient program to find the sum of contiguous subarray
 # within a one-dimensional array of numbers which has the largest
 # sum
+# For example,
+# For array {-2, -3, 4, -1, -2, 1, 5, -3}
+# Largest is 4 + (-1) + (-2) + 1 + 5 = 7
