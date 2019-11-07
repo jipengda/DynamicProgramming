8c44bc1178e9f6307186bb8101e40b9770bef2e6
jipengda@buffalo.edu <jipengda@buffalo.edu>
2019-11-07 00:23:14 -0500

    Atom

--- CookTop.py
+++ CookTop.py
@@ -6,12 +6,24 @@ def list_all_sets(graph, timeInfor, n, iteration):
     sets=[]
     row = source[-1]
     node = -1
+    labelNumber=0 # I use labelNumbet to store feasible route label number
+    labelInforTime=0 # I use labelInforTime to store feasible route time
+    labelInforCost=0 # I use labelInforTime to store feasible route cost
     for i in graph[row]:
+        labelInforTime=0
+        labelInforCost=0
         set1 = source
         node = node + 1
         if(i != 0):
             set1 = source + [node]
+            labelNumber = labelNumber + 1
+            labelInforTime = labelInforTime + i
+            labelInforCost = labelInforCost + i
+            labelInforTime = max(labelInforTime, timeInfor[node][0])
             sets.append(set1)
+            print("label number:", labelNumber)
+            print("time is ",labelInforTime, "cost is ", labelInforCost)
+            print(set1)
     for j in range(iteration-1):
             sets_copy = sets
             sets=[]
@@ -19,21 +31,36 @@ def list_all_sets(graph, timeInfor, n, iteration):
                 row = source[-1]
                 node = -1
                 for i in graph[row]:
+                    labelInforTime = 0
+                    labelInforCost = 0
                     set1 = source
                     node = node + 1
                     if(i != 0):
                         set1 = source + [node]
-                        trueTableValue = criterion(graph, timeInfor, source, set1, node, n)
+                        trueTableValue = criterion(graph, timeInfor, source, set1, node, requests)
                         if trueTableValue is False:
+                            labelNumber = labelNumber + 1
                             sets.append(set1) # I need to edit scipt around here
+                            print("label number:",labelNumber)
+                            print(set1)
+                            memory=set1
+                            length=len(memory)
+                            labelInforTime=timeInfor[0][0]
+                            for i in range(length-1):
+                                m=memory[i]
+                                n=memory[i+1]
+                                labelInforTime=labelInforTime + graph[m][n]
+                                labelInforCost = labelInforCost + graph[m][n]
+                                labelInforTime = max(labelInforTime,timeInfor[n][0])
+                            print("time is ",labelInforTime, "cost is ",labelInforCost)
     return sets
 
-def criterion(graph, timeInfor, source, set1, node, n):
+def criterion(graph, timeInfor, source, set1, node, requests):
     trueTableValue = False
     if node in source:
         trueTableValue = True
         return trueTableValue
-    if node-n not in source and node >= n:
+    if node-n not in source and node >= requests: # I hope to stop destination 5 appear early before iteration 5
         trueTableValue = True
         return trueTableValue
     memory=set1
@@ -62,7 +89,6 @@ def criterion(graph, timeInfor, source, set1, node, n):
     return trueTableValue
 if __name__ == '__main__':
     requests = 2
-    n = requests
     iteration = 5
     graph = [[0,2,3,0,0,0],
              [0,0,4,5,4,0],
@@ -76,4 +102,4 @@ if __name__ == '__main__':
                [11,21],
                [12,22],
                [0,30]]
-    sets = list_all_sets(graph, timeInfor, n, iteration)
+    sets = list_all_sets(graph, timeInfor, requests, iteration)
