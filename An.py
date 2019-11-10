e1cb049f8809a6b02f9c8e5bbfbd985c658650d5
Jipeng Dai <jipeng@jipengs-mbp.home>
2019-11-10 14:31:02 -0500

    Niagara

---
+++ An.py
@@ -0,0 +1,3 @@
+# I hope to get my first paper published as soon as possilbe
+# And this weekend, I hope to finish 40 paper reading.
+# I hope so.
---
+++ name.py
@@ -0,0 +1,54 @@
+# Here i am going to list down all 40 papers i read today as Sun
+# November 10,2019
+
+#1:
+#Lagoudakis, Michail G., Marc Berhault, Sven Koenig,
+#Pinar Keskinocak, and Anton J. Kleywegt.
+#"Simple auctions with performance guarantees
+#for multi-robot task allocation."
+#In 2004 IEEE/RSJ International Conference
+#on Intelligent Robots and Systems (IROS)
+#(IEEE Cat. No. 04CH37566), vol. 1, pp. 698-705. IEEE, 2004.
+# my evaluation: prime allocation
+
+#2:
+#Liu, Yaxin, Richard Goodwin, and Sven Koenig.
+#"Risk-averse auction agents."
+#In Proceedings of the second international joint conference
+#on Autonomous agents and multiagent systems, pp. 353-360. ACM, 2003.
+# I hope to check again since it mentioned dynamic programming
+
+#3:
+#Johnson, Luke, Sameera Ponda, Han-Lim Choi, and Jonathan How.
+#"Asynchronous decentralized task allocation for dynamic environments."
+#In Infotech@ Aerospace 2011, p. 1441. 2011.
+# I don't think this is that important, CBBA as it has been verited
+# CBBA is not a good idea for my project
+
+#4:
+#Choi, Han-Lim, Andrew K. Whitten, and Jonathan P. How.
+#"Decentralized task allocation for heterogeneous teams with cooperation
+#constraints." In Proceedings of the 2010 American Control Conference,
+#pp. 3057-3062. IEEE, 2010.
+#Another paper related to CBBA
+
+#5:
+#Whitten, Andrew K., Han-Lim Choi, Luke B. Johnson, and Jonathan P. How.
+#"Decentralized task allocation with coupled constraints in complex missions."
+#In Proceedings of the 2011 American Control Conference, pp. 1642-1649. IEEE, 2011.
+#Very interesting, it may give some hints on elimination criterion related to my project
+
+#6:
+#Lagoudakis, Michail G., Marc Berhault, Sven Koenig, Pinar Keskinocak, and Anton J. Kleywegt.
+#"Simple auctions with performance guarantees for multi-robot task allocation."
+#In 2004 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)
+#(IEEE Cat. No. 04CH37566), vol. 1, pp. 698-705. IEEE, 2004.
+# sama as #1
+
+#7:
+#M.Liu, H.Ma, J.Li and S.Koenig.”Task and Path Planning for Multi-Agent Pickup and Delivery.”
+#In Proceedings of the International Joint Conference on Autonomous Agents and Multiagent Systems(AAMAS),
+#pages(in print), 2019
+# Very exciting paper, i think it may give a lot of help for my project
+
+# I want to go to pool deck for a rest, i will come back for rest paper.
