09da3bfa5651aea3dd524620157aa9091946a92d
Dai <jipengda@buffalo.edu>
2019-11-10 16:09:03 -0500

    Time 20s

--- name.py
+++ name.py
@@ -52,3 +52,48 @@
 # Very exciting paper, i think it may give a lot of help for my project
 
 # I want to go to pool deck for a rest, i will come back for rest paper.
+# Pool deck is closed because of children games event, instead I played basketball in the Triple gym
+
+#8:
+#Gerkey, Brian P., and Maja J. Matarić. "A formal analysis and taxonomy of task allocation in multi-robot systems."
+#The International Journal of Robotics Research 23, no. 9 (2004): 939-954.
+#A good review for approaching start the introduction of task allocation
+
+#9:
+#Zlot, R., and Anthony Stentz.”Market-based multirobot coordination for complex tasks.”
+#The International Journal of Robotics Research, 25(1), pp.73-101
+# No need to dig in. It is related to inherent struction of complex tasks, which is not my work.
+
+#10
+#]Khamis, Alaa, Ahmed Hussein, and Ahmed Elmogy. "Multi-robot task allocation:
+#A review of the state-of-the-art."
+#In Cooperative Robots and Sensor Networks 2015, pp. 31-51. Springer, Cham, 2015.
+#A good very literature review of MRTA and OTA, but not mention detailed algorithm about task allocation
+
+#11:
+#]Korsah, G. Ayorkor, Anthony Stentz, and M. Bernardine Dias.
+#"A comprehensive taxonomy for multi-robot task allocation."
+#The International Journal of Robotics Research 32, no. 12 (2013): 1495-1512.
+#still taxonomy, not related to detailed algorithm
+
+#12:
+# Gerkey, Brian Paul. "On multi-robot task allocation." (2003).
+# A very good dissertation to acknolwdege MRTA problem from a student's perspective
+
+#13:
+# Van Den Berg, Jur P., and Mark H. Overmars. "Prioritized motion planning for multiple robots."
+# In 2005 IEEE/RSJ International Conference on Intelligent Robots and Systems, pp. 430-435. IEEE, 2005
+# It maybe helpful since it mentions A* method.
+
+#14:
+# Najafabadi, SayedJalil Modares. "Designing Multi-Drone Networks and Applications."
+# PhD diss., State University of New York at Buffalo, 2017.
+# It exclusively introducte the history of my project, and also it is from the same lab as mine. Very precious.
+
+#15:
+# Leung, Keith YK, Timothy D. Barfoot, and Hugh HT Liu.
+# "Decentralized localization of sparsely-communicating robot networks: A centralized-equivalent approach."
+# IEEE Transactions on Robotics 26, no. 1 (2010): 62-77.
+# Connectivity among robots. Not related.
+
+# I want to take rest for grading EE202A, prepared in advance.
