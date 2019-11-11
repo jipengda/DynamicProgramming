536ebbf0cb7f1c4681d0424608eac05dc315e034
Jipeng Dai <jipeng@jipengs-mbp.home>
2019-11-10 21:12:09 -0500

    hAPPEN

--- name.py
+++ name.py
@@ -103,3 +103,95 @@
 #"Cooperative decentralized localization using scheduled wireless transmissions."
 #IEEE Communications Letters 17, no. 6 (2013): 1240-1243.
 # Localization rather than task allocation. Discard
+
+#17:
+#Rabbat, Michael G., and Robert D. Nowak.
+#"Decentralized source localization and tracking [wireless sensor networks]."
+#In 2004 IEEE International Conference on Acoustics, Speech, and Signal Processing,
+#vol. 3, pp. iii-921. IEEE, 2004.
+# similar as 16, related to localization rather than task allocation. Discard
+
+#18:
+#Spletzer, J., Aveek K. Das, Rafael Fierro, Camillo J. Taylor, Vijay Kumar, and James P. Ostrowski.
+#"Cooperative localization and control for multi-robot manipulation."
+#In Proceedings 2001 IEEE/RSJ International Conference on Intelligent Robots and Systems.
+#Expanding the Societal Role of Robotics in the the Next Millennium (Cat. No. 01CH37180),
+#vol. 2, pp. 631-636. IEEE, 2001.
+#It mainly multirobts capture one object in physical operations. This not like my work, which is
+# in order to achieve minimum cost by using multigple robots finishing specified tasks
+
+#19:
+# Carrillo-Arce, Luis C., Esha D. Nerurkar, José L. Gordillo, and Stergios I. Roumeliotis.
+#"Decentralized multi-robot cooperative localization using covariance intersection."
+#In 2013 IEEE/RSJ International Conference on Intelligent Robots and Systems, pp. 1412-1417. IEEE, 2013.
+# Still localization, which is not that related
+
+#20:
+# Ponda, Sameera S., Luke B. Johnson, Andrew N. Kopeikin, Han-Lim Choi, and Jonathan P. How.
+# "Distributed planning strategies to ensure network connectivity for dynamic heterogeneous teams."
+# IEEE Journal on Selected Areas in Communications 30, no. 5 (2012): 861-869.
+# Very exciting paper, worth the second reading, I hope it is related well to my project.
+
+#21:
+# E. Galceran and M. Carreras. A survey on coverage path planning for robotics.
+# Robotics and Autonomous Systems,61(12):1258–1276, 2013.
+# This review is very good. It may give me instructions on how to make correct elimination criterion.
+
+#22:
+# H. Choset. Coverage for robotics–a survey of recent results.
+# Annals of mathematics and artificial intelligence, 31(1-4):113–126, 2001.
+#thIS Paper explaints four types to map processing. i believe it is helpful
+
+#23:
+# E. M. Arkin, S. P. Fekete, and J. S. Mitchell. Approximation algorithms for lawn mowing and milling.
+# Computational Geometry, 17(1-2):25–50, 2000.
+# I may get hints from it about how to evade obstables
+
+
+#24:
+# C. H. Papadimitriou. The Euclidean traveling salesman problem is np-complete.
+# Theoretical Computer Science, 4(3):237–244, 1977.
+# This one is more theorotical. You can find arbitrary reference here for dependency.
+
+#25:
+# Y. Gabriely and E. Rimon. Spanning-tree based coverage of continuous areas by a mobile robot.
+# Annals of Mathematics and Artificial Intelligence (AMAI), 31(1-4):77–98, 2001.
+# i believe this mehod spanning tree coverage is what mk told me.It is very exciting
+# i should get powerful help from this. Double check!
+
+#26:
+#N. Agmon, N. Hazon, G. A. Kaminka, M. Group, et al.
+#The giving tree: constructing trees for efficient offline and online multi-robot coverage.
+#Annals of Mathematics and Artificial Intelligence (AMAI), 52(2-4):143–168, 2008
+#This paper is interesting.It TALKS about spinning tree.It MIGHT give help as an
+#alternative to dynamic programming
+
+#27:
+# R. Yehoshua and N. Agmon. Multi-robot adversarial coverage.
+# In Proceedings of the European Conference on Artificial Intelligence (ECAI), pages 1493–1501, 2016.
+# Adversial coverage may not be related to my project.But this paper talks about a lot
+# about detailed algorithm about task allocation. May need double check.
+
+#28:
+# Torres, Marina, David A. Pelta, José L. Verdegay, and Juan C. Torres.
+# "Coverage path planning with unmanned aerial vehicles for 3D terrain reconstruction."
+# Expert Systems with Applications 55 (2016): 441-451.
+# good paper, but 3D terrain is not related to my project
+
+#29:
+# Carmel, David, and Shaul Markovitch.
+# "The M* algorithm: Incorporating opponent models into adversary search."
+# Technical Report CIS9402. Technion Haifa, Israel, 1994.
+# I don't think it is relalted
+
+#30:
+# Hart, P.E., Nilsson, N.J. and Raphael, B., 1968.
+# A formal basis for the heuristic determination of minimum cost paths.
+# IEEE transactions on Systems Science and Cybernetics, 4(2), pp.100-107.
+# Very instructive paper. It can give help to deal with cases with obastacles
+
+#31:
+# Wang, Ko-Hsin Cindy, and Adi Botea.
+# "MAPP: a scalable multi-agent path planning algorithm with tractability and completeness guarantees."
+# Journal of Artificial Intelligence Research 42 (2011): 55-90.
+# This paper is very strange. It seems good, but i am not sure.
