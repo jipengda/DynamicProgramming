57f4772a8d31ba1cb4f47878cfd5b9fb4df20bad
Dai <jipengda@buffalo.edu>
2019-11-12 19:05:38 -0500

    Dr. Mastronarde

--- Lowe's.py
+++ Lowe's.py
@@ -40,8 +40,10 @@ def list_all_sets(graph, timeInfor, requests, iteration):
                     if(i != 0):
                         set1 = source + [node]
                         trueTableValue = criterion(graph, timeInfor, source, set1, node, requests)
+                        if(node==iteration and j!=iteration-2):
+                            trueTableValue = True # I hope thest two lines give a good shot
                         if trueTableValue is False:
-                            labelNumber = labelNumber + 1
+#                            labelNumber = labelNumber + 1 #I hope change this order to end works
                             memory=set1
                             length=len(memory)
                             labelInforTime=timeInfor[0][0]
@@ -53,13 +55,37 @@ def list_all_sets(graph, timeInfor, requests, iteration):
                                 labelInforTime = max(labelInforTime,timeInfor[n][0])
                             # set_printing=set1+[[labelInforTime, labelInforCost]]
                             set1 = set1+[[labelInforTime, labelInforCost]]
-                            sets.append(set1) # I need to edit scipt around here
-                            print("label number:",labelNumber)
+                            # Here I need to judge set1 using fellow_lable function before it can be appended to sets
+                            # I need to make changes in fellow_label function to get used to variables with new format
+                            delete_flag=fellow_label(sets,set1)
+                            if(delete_flag==-2): # Before this, I want to test fellow_lable function first
+                            # I don't think I need make changes after verification
+                                sets.append(set1) # I need to edit scipt around here
+#                                print("label number:",labelNumber)
                             # print(set_printing)
-                            print(set1)
+#                                print(set1)
                             # Accrodingly, I need to change criterion function
+                            # OK, I think I am finished. why not have a test?
+                            elif(delete_flag==-1):
+                                pass
+                            else:
+                                del sets[delete_flag]
+                                sets.append(set1)
+#                                print("label number",labelNumber)
+#                                print(set1)
+                                # the number delete_flag label can be kicked out
+                                # I hope it works now
+                                # I need to print sets included the label number here for each iteration.
+#            print("iteration is ", j+2)
+            for print_set in sets:
+                labelNumber = labelNumber + 1
+                print("label number:", labelNumber)
+                print(print_set)
+#            print(sets)
+
     return sets
     # Let me have a test
+    # It works. Same as the paper. Good job!
 
 def criterion(graph, timeInfor, source, set1, node, requests):
     trueTableValue = False
@@ -95,36 +121,57 @@ def criterion(graph, timeInfor, source, set1, node, requests):
     return trueTableValue
 
 def fellow_label(sets, new_one):
-    new_one = sets[-1]
-    new_one_time = new_one[0]
-    new_one_cost = new_one[1]
+    #new_one = sets[-1]
+    View=len(new_one)
+    Tool=View-1
+    new_one_nodes=new_one[0:Tool]
+    new_one_time = new_one[-1][0]
+    new_one_cost = new_one[-1][1]
     length=len(sets)
-    delete_flag=0
+    delete_flag=-2
     # Here, every set has format like [node x1, node x2, node x3, node x4, node x5, [time, cost]]
     # time compare
-    for i in range(length-1):
+    for i in range(length):
         compare=sets[i]
-        compare_time=compare[0]
-        compare_cost=compare[1]
+        compare_time=compare[-1][0]
+        compare_cost=compare[-1][1]
         if (new_one_time == compare_time):
             if(new_one_cost > compare_cost):
-                delete_flag=1
+                delete_flag=-1
                 return delete_flag
 
     # if time is equal, compare the cost, if it has more cost, delete flag = 1, default delete flag=0, means does not delete
 
     # cost compare
-    for i in range(length-1):
+    for i in range(length):
         compare=sets[i]
-        compare_time=compare[0]
-        compare_cost=compare[1]
+        compare_time=compare[-1][0]
+        compare_cost=compare[-1][1]
         if (new_one_cost == compare_cost):
             if(new_one_time > compare_time):
-                delete_flag=1
+                delete_flag=-1
                 return delete_flag
-    return delete_flag
+
+    # see if new_one can kick some old one. If so, record index of old one
+    for i in range(length):
+        compare=sets[i]
+        View=len(compare)
+        Tool=View-1
+        compare_nodes=compare[0:Tool]
+        compare_time=compare[-1][0]
+        compare_cost=compare[-1][1]
+        if(new_one_cost<=compare_cost):
+            if(new_one_time<=compare_time):
+                if( set(new_one_nodes)==set(compare_nodes) ):
+                    delete_flag=i # it means ith label can be kicked out by new_label
+                # i can be considered to kick out
+                    return delete_flag
+    return delete_flag#34-47
+    # delete_flag = -2 means new_one can be added
+    # delete_flag = -1 means new_one can not be added
+    # delete_flag = other means new_one can be added and kick out the other(th) label
 if __name__ == '__main__':
-    iteration = 1
+    iteration = 5
     requests = 2
     bigNumber = 1000
     diagonal = 1.414 # diagonal distance
@@ -142,3 +189,12 @@ if __name__ == '__main__':
                [12,22],
                [0,30]]
     sets = list_all_sets(graph, timeInfor, requests, iteration)
+    # Here comes a new question, if the bad one enters sets in first order, we cannot delete it during
+    # elimination judgement
+    # Then my question is how to deal with that?
+    # Should I deal with sets again after adding new valid label?
+    # I think so
+    # Or, after get sets, excecute an action, if two sets have same elements, the weaker one get deleted
+    # I feel so glad the final result works now same with the paper.
+    # But in the future, jingyiqiujing, something can be directed to deal with the destination
+    # show up early issue.
--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-# In this file, I hope to create something related to my EECPP problem referring to CookTop.py
-# And also, tomorrow I want to ask Doctors about especially Dr. Kang about requests, node 5 problems
-# Why not start it now?
-# Firstly, I think I need to define my problem
-# What is my start node?
-# What is my destination?
-# What is my adjacency matrix?
-# What is my map?
-# Less but not last, what is my criterions in EECPP to eliminate redundant state?
-# I hope I can have an idea about those today!
-
-
-# I must figure out the definition of requests first so that I can change it to get used to new problems.
-# I hope to get answer as soon as possible
-
 def list_all_sets(graph, timeInfor, requests, iteration):
     source=[0]
     sets=[]
@@ -21,7 +6,7 @@ def list_all_sets(graph, timeInfor, requests, iteration):
     labelNumber=0 # I use labelNumbet to store feasible route label number
     labelInforTime=0 # I use labelInforTime to store feasible route time
     labelInforCost=0 # I use labelInforTime to store feasible route cost
-    for i in graph[row]:
+    for i in graph[row]:# Maybe I can do something helpful here
         labelInforTime=0
         labelInforCost=0
         set1 = source
@@ -32,20 +17,25 @@ def list_all_sets(graph, timeInfor, requests, iteration):
             labelInforTime = labelInforTime + i
             labelInforCost = labelInforCost + i
             labelInforTime = max(labelInforTime, timeInfor[node][0])
-            set_printing=set1+[[labelInforTime, labelInforCost]]
-            sets.append(set1)
+            # set_printing=set1+[[labelInforTime, labelInforCost]]
+            set1 = set1+[[labelInforTime, labelInforCost]]
+            sets.append(set1) # why not use set1 new instead?
             print("label number:", labelNumber)
-            print(set_printing)
+            # print(set_printing)
+            print(set1)
     for j in range(iteration-1):
             sets_copy = sets
             sets=[]
             for source in sets_copy:
-                row = source[-1]
+                #row = source[-1] #This needs changing since format is changing
+                row = source[-2] #-2 is last visiting node, -1 is time and cost infor
                 node = -1
+                lastElement = source.pop()
+                set1 = source
                 for i in graph[row]:
                     labelInforTime = 0
                     labelInforCost = 0
-                    set1 = source
+                    #change here thanks to formate changing. use last element to store
                     node = node + 1
                     if(i != 0):
                         set1 = source + [node]
@@ -61,11 +51,30 @@ def list_all_sets(graph, timeInfor, requests, iteration):
                                 labelInforTime=labelInforTime + graph[m][n]
                                 labelInforCost = labelInforCost + graph[m][n]
                                 labelInforTime = max(labelInforTime,timeInfor[n][0])
-                            set_printing=set1+[[labelInforTime, labelInforCost]]
-                            sets.append(set1) # I need to edit scipt around here
-                            print("label number:",labelNumber)
-                            print(set_printing)
+                            # set_printing=set1+[[labelInforTime, labelInforCost]]
+                            set1 = set1+[[labelInforTime, labelInforCost]]
+                            # Here I need to judge set1 using fellow_lable function before it can be appended to sets
+                            # I need to make changes in fellow_label function to get used to variables with new format
+                            delete_flag=fellow_label(sets,set1)
+                            if(delete_flag==-2): # Before this, I want to test fellow_lable function first
+                            # I don't think I need make changes after verification
+                                sets.append(set1) # I need to edit scipt around here
+                                print("label number:",labelNumber)
+                            # print(set_printing)
+                                print(set1)
+                            # Accrodingly, I need to change criterion function
+                            # OK, I think I am finished. why not have a test?
+                            elif(delete_flag==-1):
+                                pass
+                            else:
+                                del sets[delete_flag]
+                                sets.append(set1)
+                                print("label number",labelNumber)
+                                print(set1)
+                                # the number delete_flag label can be kicked out
+                                # I hope it works now
     return sets
+    # Let me have a test
 
 def criterion(graph, timeInfor, source, set1, node, requests):
     trueTableValue = False
@@ -99,6 +108,57 @@ def criterion(graph, timeInfor, source, set1, node, requests):
                 trueTableValue = True
                 return trueTableValue
     return trueTableValue
+
+def fellow_label(sets, new_one):
+    #new_one = sets[-1]
+    View=len(new_one)
+    Tool=View-1
+    new_one_nodes=new_one[0:Tool]
+    new_one_time = new_one[-1][0]
+    new_one_cost = new_one[-1][1]
+    length=len(sets)
+    delete_flag=-2
+    # Here, every set has format like [node x1, node x2, node x3, node x4, node x5, [time, cost]]
+    # time compare
+    for i in range(length):
+        compare=sets[i]
+        compare_time=compare[-1][0]
+        compare_cost=compare[-1][1]
+        if (new_one_time == compare_time):
+            if(new_one_cost > compare_cost):
+                delete_flag=-1
+                return delete_flag
+
+    # if time is equal, compare the cost, if it has more cost, delete flag = 1, default delete flag=0, means does not delete
+
+    # cost compare
+    for i in range(length):
+        compare=sets[i]
+        compare_time=compare[-1][0]
+        compare_cost=compare[-1][1]
+        if (new_one_cost == compare_cost):
+            if(new_one_time > compare_time):
+                delete_flag=-1
+                return delete_flag
+
+    # see if new_one can kick some old one. If so, record index of old one
+    for i in range(length):
+        compare=sets[i]
+        View=len(compare)
+        Tool=View-1
+        compare_nodes=compare[0:Tool]
+        compare_time=compare[-1][0]
+        compare_cost=compare[-1][1]
+        if(new_one_cost<=compare_cost):
+            if(new_one_time<=compare_time):
+                if( set(new_one_nodes)==set(compare_nodes) ):
+                    delete_flag=i # it means ith label can be kicked out by new_label
+                # i can be considered to kick out
+                    return delete_flag
+    return delete_flag#34-47
+    # delete_flag = -2 means new_one can be added
+    # delete_flag = -1 means new_one can not be added
+    # delete_flag = other means new_one can be added and kick out the other(th) label
 if __name__ == '__main__':
     iteration = 5
     requests = 2
@@ -118,13 +178,9 @@ if __name__ == '__main__':
                [12,22],
                [0,30]]
     sets = list_all_sets(graph, timeInfor, requests, iteration)
-
-# This verification matches my expectation(a point, if the graph has no feasible route to visit up all nodes, what to do)
-# (Do i need change sth of elimination criterion)
-# What's most important, how to define my EECPP map to get used to this code, and use this code to get the same result as
-# I use Cplex.
-# I need to change the also elimination criterion because I don't have time constraint, and I need to add turning cost
-# and I only have one drone
-# These are too much to handle for today. I am so hungery that I need to go back home before 4 o'clock.
-# I wonder to find out the optimal route in each iteration, time cost or cost, the minimum
-# This is why previous 5<-4<-3<-2<-1<-0 is eliminated. This one not solved in my script.
+    # Here comes a new question, if the bad one enters sets in first order, we cannot delete it during
+    # elimination judgement
+    # Then my question is how to deal with that?
+    # Should I deal with sets again after adding new valid label?
+    # I think so
+    # Or, after get sets, excecute an action, if two sets have same elements, the weaker one get deleted
