827a8d0896c1ecd94c0ad7f791025f6d46b44bbc
Dai <jipengda@buffalo.edu>
2019-11-14 11:19:34 -0500

    Discover

---
+++ Chestnut.py
@@ -0,0 +1,8 @@
+# Today I hope to make a general framework for one drone EECPP problem
+# This project is slightly different from project in the paper
+# I need to be considering well before action
+
+# I want to define a test map first
+# It will be a grid map of nodes, 14 nodes at most at this moment
+# Probably I can refer figure in the paer, try to regard as physical
+# object
